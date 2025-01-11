d546b91c356b0fa1b8aa006a2f95a93eabc34b5c
sisemochi <silvia.samfirescu@gmail.com>
2025-01-11 21:06:46 +0200

    setup custom paper theme

--- rn-adjust-a-dish/app/_layout.tsx
+++ rn-adjust-a-dish/app/_layout.tsx
@@ -1,67 +1,76 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import {DarkTheme, DefaultTheme, ThemeProvider} from '@react-navigation/native';
+import {useFonts} from 'expo-font';
+import {Stack} from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/components/useColorScheme';
-import {PaperProvider} from "react-native-paper";
+import {useColorScheme} from '@/components/useColorScheme';
+import {MD3DarkTheme, MD3LightTheme, PaperProvider} from "react-native-paper";
+import {defaultDarkTheme, defaultLightTheme} from "@/constants/Colors";
 
 export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
+    // Catch any errors thrown by the Layout component.
+    ErrorBoundary,
 } from 'expo-router';
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: 'index',
+    // Ensure that reloading on `/modal` keeps a back button present.
+    initialRouteName: 'index',
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
+    const [loaded, error] = useFonts({
+        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+        ...FontAwesome.font,
+    });
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+    const colorScheme = useColorScheme();
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    const paperTheme = colorScheme === "light" ? {
+        ...MD3LightTheme,
+        colors: defaultLightTheme.colors
+    } : {...MD3DarkTheme, colors: defaultDarkTheme.colors}
+
+
+    // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+    useEffect(() => {
+        if (error) throw error;
+    }, [error]);
+
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
     }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-      <PaperProvider>
-        <RootLayoutNav />
-      </PaperProvider>
-  );
+    return (
+        <PaperProvider theme={paperTheme}>
+            <RootLayoutNav/>
+        </PaperProvider>
+    );
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme();
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="index" options={{headerShown:false}} />
-        <Stack.Screen name="login" options={{headerBackTitle:"Back", title: "Login"}} />
-        <Stack.Screen name="register" options={{headerBackTitle:"Back", title: "Sign up"}} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+            <Stack>
+                <Stack.Screen name="index" options={{headerShown: false}}/>
+                <Stack.Screen name="login" options={{headerBackTitle: "Back", title: "Login"}}/>
+                <Stack.Screen name="register" options={{headerBackTitle: "Back", title: "Sign up"}}/>
+                <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+                <Stack.Screen name="modal" options={{presentation: 'modal'}}/>
+            </Stack>
+        </ThemeProvider>
+    );
 }
